f3be944532342e69bf8c21b0dd89b665b09cd44f
kevserozkan <sumeyye.kevser.ozkan@finartz.com>
2023-08-25 11:44:40 +0300

    feat: removed all unnecessary codes and pages.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,63 +1,14 @@
-import { Link, Typography } from "@mui/material";
-
-import {React, useContext} from "react";
-import { useNavigate } from "react-router-dom";
-
 import "../Styles.css";
 
-function Footer(props) {
-  
-    const history = useNavigate();
-
+const Footer = () => {
 
   return (
     <>
-      <div class="footer">
-        <div class="footer-item">
-            <Typography variant="h5" >Sayfalar</Typography>
-          <Link
-            component="button"
-            variant="h6"
-            onClick={() => {
-              history("/");
-            }}
-          >
-            Anasayfa
-          </Link>
-          <Link
-            component="button"
-            variant="h6"
-            onClick={() => {
-              history("/Hakkımızda");
-            }}
-          >
-            Hakkımızda
-          </Link>
-          <Link
-            component="button"
-            variant="h6"
-            onClick={() => {
-              history("/");
-            }}
-          >
-            Kurumlar
-          </Link>
-        </div>
-        <div class="footer-item">
-            <Typography variant="h5"> Sayfalar </Typography>
-          <Link component="button" variant="h6">
-            Anasayfa
-          </Link>
-          <Link component="button" variant="h6">
-            Hakkımızda
-          </Link>
-          <Link component="button" variant="h6">
-            Kurumlar
-          </Link>
-        </div>
+      <div className="footer">
+Footer goes here..
       </div>
     </>
-  );
+  )
 }
 
 export default Footer;
--- src/Page/Main.jsx
+++ src/Page/Main.jsx
@@ -1,70 +1,13 @@
-import React, { useContext } from "react";
+import React  from "react";
 
 import "../Styles.css";
 
-import Typography from "@mui/material/Typography";
-import { DataContext } from "../Data/DataContext";
-
-import ContentBox from "../Components/ContentBox";
-import  Gmap from "../Components/Gmap";
-
-var content = [
-  "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir  ",
-];
-
-function MainPage(props) {
-  const { theme } = useContext(DataContext);
+function MainPage() {
 
   return (
-    <>
-      <div
-        className={theme ? `pageTemplate pageTemplate-dark` : `pageTemplate`}
-      >
-        <div className="midcontent">
-          <Typography variant="h4"> Anasayfa </Typography>
-          <div className="mid">
-            <Gmap />
-
-            <ContentBox
-              className={
-                theme ? `pageTemplate pageTemplate` : `pageTemplate-dark`
-              }
-              content={content}
-            />
-            <ContentBox
-              className={
-                theme ? `pageTemplate pageTemplate` : `pageTemplate-dark`
-              }
-              content={content}
-            />
-            <ContentBox
-              className={
-                theme ? `pageTemplate pageTemplate` : `pageTemplate-dark`
-              }
-              content={content}
-            />
-            <ContentBox
-              className={
-                theme ? `pageTemplate pageTemplate` : `pageTemplate-dark`
-              }
-              content={content}
-            />
-            <ContentBox
-              className={
-                theme ? `pageTemplate pageTemplate` : `pageTemplate-dark`
-              }
-              content={content}
-            />
-            <ContentBox
-              className={
-                theme ? `pageTemplate pageTemplate` : `pageTemplate-dark`
-              }
-              content={content}
-            />
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+Content goes here...
+    </div>
   );
 }
 
