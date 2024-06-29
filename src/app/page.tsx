ae8442aa2a4f4748816674ab61ac71705cc384db
NakreS <87872407+NNakreSS@users.noreply.github.com>
2024-06-29 20:45:05 +0300

    AYS-277 | Replacing Ant Design library with Shadcn UI (#54)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { useEffect } from 'react'
 import { increment } from '@/modules/counter/counterSlice'
-import { Button } from 'antd'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   const { t } = useTranslation()
@@ -16,14 +16,21 @@ export default function Home() {
   }, [dispatch])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24 space-y-10">
       <h1 className="text-6xl font-bold text-center">
         {t('landingInProgress')}
       </h1>
-      {/*
-       <h2>{counter}</h2>
-      <Button onClick={() => dispatch(increment())}>{t('increment')}</Button>
-      */}
+
+      <div className="flex flex-col items-center gap-5 w-1/4">
+        <h2>{counter}</h2>
+        <Button
+          variant="default"
+          className="w-full"
+          onClick={() => dispatch(increment())}
+        >
+          {t('increment')}
+        </Button>
+      </div>
     </main>
   )
 }
