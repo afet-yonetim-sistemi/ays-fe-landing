dac58a5689ebcffd9858523bbbc805d13fb7af60
NNakreSS <serkanatmaca15@gmail.com>
2024-07-06 22:29:14 +0300

    Link urls added

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,13 +1,14 @@
 import { FaConfluence, FaDiscord, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { SiJira } from 'react-icons/si'
 import Link from 'next/link'
 
 export default function SocialIcons() {
   return <div className="flex items-center justify-center w-full gap-4 text-xl text-green">
-    {/*TODO: Linkler yerleştirilecek*/}
-    <Link href={''}><FaLinkedin /></Link>
-    <Link href={''}><FaDiscord /></Link>
-    <Link href={''}><FaConfluence /></Link>
-    <Link href={''}><FaGithub /></Link>
+    <Link href={'https://www.linkedin.com/company/afetyonetimsistemi/'}><FaLinkedin /></Link>
+    <Link href={'https://discord.gg/NkAkYajkKa'}><FaDiscord /></Link>
+    <Link href={'https://afetyonetimsistemi.atlassian.net/jira'}><SiJira /></Link>
+    <Link href={'https://afetyonetimsistemi.atlassian.net/wiki'}><FaConfluence /></Link>
+    <Link href={'https://github.com/afet-yonetim-sistemi'}><FaGithub /></Link>
   </div>
 }
 
