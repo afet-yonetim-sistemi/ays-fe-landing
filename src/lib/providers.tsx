feb1453d470b1d13b038a9bb1c88aa6da2b6a676
NNakreSS <serkanatmaca15@gmail.com>
2024-07-06 17:31:01 +0300

    theme provider added

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -3,11 +3,19 @@
 import { I18nextProvider } from 'react-i18next'
 import i18n from '@/i18n'
 import StoreProvider from '@/store/StoreProvider'
+import { ThemeProvider } from '@/components/ui/theme-provider'
 
 export const Providers = (props: React.PropsWithChildren) => {
   return (
-    <I18nextProvider i18n={i18n} defaultNS={'translation'}>
-      <StoreProvider>{props.children}</StoreProvider>
-    </I18nextProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <I18nextProvider i18n={i18n} defaultNS={'translation'}>
+        <StoreProvider>{props.children}</StoreProvider>
+      </I18nextProvider>
+    </ThemeProvider>
   )
 }
