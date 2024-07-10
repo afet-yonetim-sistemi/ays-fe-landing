493bb60c7e5ef4fcbf2ae56463204cec5afe8200
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 14:58:48 +0300

    target _blank added

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 
 export default function SocialIcons() {
   return <div className="flex items-center justify-center w-full gap-4 text-xl text-green">
-    <Link href={'https://www.linkedin.com/company/afetyonetimsistemi/'}><FaLinkedin /></Link>
-    <Link href={'https://discord.gg/NkAkYajkKa'}><FaDiscord /></Link>
-    <Link href={'https://afetyonetimsistemi.atlassian.net/jira'}><SiJira /></Link>
-    <Link href={'https://afetyonetimsistemi.atlassian.net/wiki'}><FaConfluence /></Link>
-    <Link href={'https://github.com/afet-yonetim-sistemi'}><FaGithub /></Link>
+    <Link target="_blank" href={'https://www.linkedin.com/company/afetyonetimsistemi/'}><FaLinkedin /></Link>
+    <Link target="_blank" href={'https://discord.gg/NkAkYajkKa'}><FaDiscord /></Link>
+    <Link target="_blank" href={'https://afetyonetimsistemi.atlassian.net/jira'}><SiJira /></Link>
+    <Link target="_blank" href={'https://afetyonetimsistemi.atlassian.net/wiki'}><FaConfluence /></Link>
+    <Link target="_blank" href={'https://github.com/afet-yonetim-sistemi'}><FaGithub /></Link>
   </div>
 }
 
