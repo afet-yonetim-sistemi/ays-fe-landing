4b4bf9f6ebb622503ffd982e16f4ea405a89278e
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 22:47:29 +0300

    added auto-play plugin

--- src/components/Hero/HeroCarousel.tsx
+++ src/components/Hero/HeroCarousel.tsx
@@ -1,19 +1,26 @@
+'use client'
+
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
 import images from './images'
 import Image from 'next/image'
 
 export default function HeroCarousel() {
   return <div className="rounded-2xl border border-nightBlue overflow-hidden">
-    <Carousel opts={
-      {
-        align: 'start',
+    <Carousel
+      opts={{
         loop: true
-      }
-    }>
+      }}
+      plugins={[
+        Autoplay({
+          delay: 3000
+        })
+      ]}
+    >
       <CarouselContent>
         {images.map(({ src, alt }, index) => (
           <CarouselItem key={index}>
-            <Image src={src} alt={alt} className="object-cover w-full" width={500} height={700} priority />
+            <Image src={src} alt={alt} className="object-cover w-full cursor-grab" width={500} height={700} priority />
           </CarouselItem>
         ))}
       </CarouselContent>
