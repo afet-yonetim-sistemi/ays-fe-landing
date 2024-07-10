6574714f79e66684c1d1b09411b889ab22fff223
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 18:48:39 +0300

    padding set

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,5 @@
 export default function Main({ children }: { children: React.ReactNode }) {
-  return <main className="flex min-h-screen flex-col items-center justify-start p-24 space-y-10">
+  return <main className="flex min-h-screen flex-col items-center justify-start px-20 space-y-10">
     {children}
   </main>
 }
