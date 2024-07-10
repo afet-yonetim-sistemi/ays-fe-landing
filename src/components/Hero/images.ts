a6213a5dd19a4d908e01cb7218e2ea0e94afebd5
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 22:23:02 +0300

    initial commit

---
+++ src/components/Hero/images.ts
@@ -0,0 +1,10 @@
+const slogansPage = '/images/slogans/'
+const images = [
+  { src: slogansPage + 'first.jpg', alt: '' },
+  { src: slogansPage + 'second.jpg', alt: '' },
+  { src: slogansPage + 'third.jpg', alt: '' },
+  { src: slogansPage + 'fourth.jpg', alt: '' },
+  { src: slogansPage + 'fifth.jpg', alt: '' }
+]
+
+export default images
