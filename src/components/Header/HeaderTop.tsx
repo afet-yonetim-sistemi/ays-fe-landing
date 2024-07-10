57dbfa34f3ac01113a2c566c8a733494606090a8
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 16:30:46 +0300

    i18n implemented

--- src/components/Header/HeaderTop.tsx
+++ src/components/Header/HeaderTop.tsx
@@ -1,9 +1,10 @@
-import Link from 'next/link'
 import Image from 'next/image'
 import SocialIcons from '@/components/SocialIcons'
 import LanguageSelector from '@/components/LanguageSelector'
+import { t } from 'i18next'
 
 export default function HeaderTop() {
+
   return <div className="flex items-center justify-between border-b-2 py-4">
     {/*slogan & logo container*/}
     <div className="flex gap-3">
@@ -11,8 +12,8 @@ export default function HeaderTop() {
         <Image src="/logo.png" alt="logo" width={60} height={60} />
       </div>
       <div className="flex flex-col items-start justify-center">
-        <span className="text-3xl font-bold">Afet Yönetim Sistemi</span>
-        <span className="font-medium text-base">SİSTEM HAYAT KURTARIR</span>
+        <span className="text-3xl font-bold">{t('header.title')}</span>
+        <span className="font-medium text-base">{t('header.subtitle')}</span>
       </div>
     </div>
     {/*slogan & logo container*/}
