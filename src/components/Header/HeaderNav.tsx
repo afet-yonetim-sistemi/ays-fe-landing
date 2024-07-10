b4b1a1693a625fb963e2161535389de7a7e0ad1e
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 16:30:39 +0300

    i18n implemented

--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -4,16 +4,18 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { clsx } from 'clsx'
 import { AiOutlineHome } from 'react-icons/ai'
+import { useTranslation } from 'react-i18next'
 
 export default function HeaderNav() {
   const pathName = usePathname()
+  const { t } = useTranslation()
   return (
     <nav className="flex items-center justify-between font-normal text-normal">
       <Link href="/" className={clsx('flex-center gap-2 py-1', {
         'border-b-2': pathName === '/' || pathName === '/home'
       })}>
         <AiOutlineHome />
-        <span>Anasayfa</span>
+        <span>{t('nav.home')}</span>
       </Link>
     </nav>
   )
