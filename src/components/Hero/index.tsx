bb9468c0e5cf8fe5e9776a73a3adbfe738333cb4
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 22:23:23 +0300

    gap sett

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,7 +2,7 @@ import HeroCarousel from './HeroCarousel'
 import EmergencyForm from './EmergencyForm'
 
 export default function HeroSection() {
-  return <section className="grid grid-cols-1 lg:grid-cols-2 w-full">
+  return <section className="grid grid-cols-1 lg:grid-cols-2 gap-5 w-full">
     <HeroCarousel />
     <EmergencyForm />
   </section>
