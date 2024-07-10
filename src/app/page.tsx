d8ef0870aa375b7e1dac833843102c10c389ca4a
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 18:49:15 +0300

    Hero component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Header from '@/components/Header'
 import Main from '@/components/main'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
     <Main>
-      {''}
+      <Hero />
     </Main>
   )
 }
