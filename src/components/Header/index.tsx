65fbf76b7b50710a348e0d83d6863b6b70bfdcaf
NNakreSS <serkanatmaca15@gmail.com>
2024-07-10 22:23:55 +0300

    container provider added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,10 @@ import HeaderTop from '@/components/Header/HeaderTop'
 import HeaderNav from '@/components/Header/HeaderNav'
 
 export default function Header() {
-  return <header className="w-full py-5 px-20 bg-nightBlue text-background space-y-4">
-    <HeaderTop />
-    <HeaderNav />
+  return <header className="w-full bg-nightBlue text-background">
+    <div className="container space-y-4 px-20 py-5">
+      <HeaderTop />
+      <HeaderNav />
+    </div>
   </header>
 }
