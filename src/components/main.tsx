7f7fffb062f2bd72f5cfeb0fba442717dfa3bc4d
NNakreSS <serkanatmaca15@gmail.com>
2024-07-13 19:30:30 +0300

    REFACTOR : removed container margin and minimum screen size from class name

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,5 @@
 export default function Main({ children }: { children: React.ReactNode }) {
-  return <main className="flex min-h-screen flex-col items-center justify-start px-20 space-y-10 my-5 container">
+  return <main className="flex flex-col items-center justify-start space-y-10">
     {children}
   </main>
 }
