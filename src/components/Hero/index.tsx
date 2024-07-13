ffd088d41f7dc6508b93407ecb961252fd5ab0bb
NNakreSS <serkanatmaca15@gmail.com>
2024-07-13 19:31:12 +0300

    REFACTOR : refactoring class & added container component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,13 @@
 import HeroCarousel from './HeroCarousel'
-import EmergencyForm from '@/components/EmergencyForm'
+import Container from '@/components/Container'
+// import EmergencyForm from '@/components/EmergencyForm'
 
 export default function HeroSection() {
-  return <section className="grid grid-cols-1 lg:grid-cols-2 gap-5 w-full">
-    <HeroCarousel />
-    <EmergencyForm />
+  return <section className="w-full bg-[#99B93A] p-5">
+    <Container className="grid grid-cols-1 gap-5">
+      <HeroCarousel />
+      {/*<EmergencyForm />*/}
+      {/*TODO : AYS-333 ile birlikte EmergencyForm entegre edilecek*/}
+    </Container>
   </section>
 }
