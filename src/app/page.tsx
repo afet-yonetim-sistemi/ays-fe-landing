85af8c8c8ce2f77bd2fbf20c0417a199f7b3c200
NNakreSS <serkanatmaca15@gmail.com>
2024-07-13 19:29:46 +0300

    added emergencyInfo & aboutUs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Header from '@/components/Header'
 import Main from '@/components/main'
 import Hero from '@/components/Hero'
+import EmergencyInfo from '../components/EmergencyInfo'
+import AboutUs from '@/components/AboutUs'
 
 export default function Home() {
   return (
     <Main>
       <Hero />
+      <EmergencyInfo />
+      <AboutUs />
     </Main>
   )
 }
