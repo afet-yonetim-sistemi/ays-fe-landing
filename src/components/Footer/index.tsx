d6219fac132c8fe0518b5ad47d9276ed5a9f24cd
NNakreSS <serkanatmaca15@gmail.com>
2024-07-13 19:31:28 +0300

    REFACTOR : refactoring class & added container component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,17 @@
 import SocialIcons from '@/components/SocialIcons'
 import LanguageSelector from '@/components/LanguageSelector'
 import i18n from 'i18next'
+import Container from '@/components/Container'
 
 export default function Footer() {
   const { t } = i18n
   return <footer className="bg-nightBlue w-full text-background">
-    <div className="container px-20 py-5 flex items-center justify-between">
+    <Container className="py-5 flex items-center justify-between">
       <span>{t('copyright')}</span>
       <div className="flex items-center justify-between gap-5">
         <SocialIcons />
         <LanguageSelector />
       </div>
-    </div>
+    </Container>
   </footer>
 }
