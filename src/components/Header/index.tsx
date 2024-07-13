0415b3e44654ff8fe14fdefea03577d23c185428
NNakreSS <serkanatmaca15@gmail.com>
2024-07-13 19:31:22 +0300

    REFACTOR : refactoring class & added container component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import HeaderTop from '@/components/Header/HeaderTop'
 import HeaderNav from '@/components/Header/HeaderNav'
+import Container from '@/components/Container'
 
 export default function Header() {
   return <header className="w-full bg-nightBlue text-background">
-    <div className="container space-y-4 px-20 py-5">
+    <Container className="space-y-4 py-5">
       <HeaderTop />
       <HeaderNav />
-    </div>
+    </Container>
   </header>
 }
