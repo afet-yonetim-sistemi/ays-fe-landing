8378e045674f3d21bcb5ccc9b2d2f87e36211c4b
NNakreSS <serkanatmaca15@gmail.com>
2024-07-13 19:32:59 +0300

    REFACTOR : refactoring class

--- src/components/Hero/HeroCarousel.tsx
+++ src/components/Hero/HeroCarousel.tsx
@@ -6,21 +6,23 @@ import images from './images'
 import Image from 'next/image'
 
 export default function HeroCarousel() {
-  return <div className="rounded-2xl border border-nightBlue overflow-hidden">
-    <Carousel
-      opts={{
-        loop: true
-      }}
-      plugins={[
-        Autoplay({
-          delay: 3000
-        })
-      ]}
+  return <div>
+    <Carousel className="cursor-grab"
+              opts={{
+                loop: true
+              }}
+              plugins={[
+                Autoplay({
+                  delay: 3000
+                })
+              ]}
     >
       <CarouselContent>
         {images.map(({ src, alt }, index) => (
           <CarouselItem key={index}>
-            <Image src={src} alt={alt} className="object-cover w-full cursor-grab" width={500} height={700} priority />
+            <Image src={src} alt={alt} className="object-contain m-auto hidden md:block" width={400} height={500}
+                   priority />
+            {/*TODO: tek satır için banner uyumlu görsellerin ayarlanması gerekli*/}
           </CarouselItem>
         ))}
       </CarouselContent>
