f5ceca36a60d34f766b790a50d192cda6ab9219e
NNakreSS <serkanatmaca15@gmail.com>
2024-07-14 16:48:33 +0300

    REFACTOR : changed font size -> xl - 2xl

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -3,7 +3,7 @@ import { SiJira } from 'react-icons/si'
 import Link from 'next/link'
 
 export default function SocialIcons() {
-  return <div className="flex items-center justify-center w-full gap-4 text-xl text-green">
+  return <div className="flex items-center justify-center w-full gap-4 text-2xl text-green">
     <Link target="_blank" href={'https://www.linkedin.com/company/afetyonetimsistemi/'}><FaLinkedin /></Link>
     <Link target="_blank" href={'https://discord.gg/NkAkYajkKa'}><FaDiscord /></Link>
     <Link target="_blank" href={'https://afetyonetimsistemi.atlassian.net/jira'}><SiJira /></Link>
