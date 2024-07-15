db1b22db84f8760e65b8034f25d68c1b8ea5c3d7
NNakreSS <serkanatmaca15@gmail.com>
2024-07-15 03:54:15 +0300

    added className prop

--- src/components/EmergencyInfo/Card.tsx
+++ src/components/EmergencyInfo/Card.tsx
@@ -2,7 +2,7 @@ import { clsx } from 'clsx'
 
 const Card = ({ className, children }: { className?: string, children: React.ReactNode }) => {
   return (
-    <article className={clsx('bg-nightBlue p-5 rounded-lg text-2xl font-medium flex-center py-10', className)}>
+      <article className={clsx('bg-nightBlue p-5 rounded-lg text-2xl font-medium flex-center py-10', className)}>
       {children}
     </article>
   )
