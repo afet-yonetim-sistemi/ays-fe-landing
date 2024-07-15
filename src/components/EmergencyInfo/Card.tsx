f447856e875f7a82d7af7d3bb46f62958b41786d
NNakreSS <serkanatmaca15@gmail.com>
2024-07-15 04:12:08 +0300

    added hover

--- src/components/EmergencyInfo/Card.tsx
+++ src/components/EmergencyInfo/Card.tsx
@@ -2,7 +2,8 @@ import { clsx } from 'clsx'
 
 const Card = ({ className, children }: { className?: string, children: React.ReactNode }) => {
   return (
-      <article className={clsx('bg-nightBlue p-5 rounded-lg text-2xl font-medium flex-center py-10', className)}>
+    <article
+      className={clsx('bg-nightBlue p-5 rounded-lg text-2xl font-medium flex-center py-10 hover:shadow-md hover:shadow-black hover:scale-[1.02] transition duration-300 select-none', className)}>
       {children}
     </article>
   )
