8d727be7ad708f119d290de8ee62cddce92dc16c
NNakreSS <serkanatmaca15@gmail.com>
2024-07-15 03:39:54 +0300

    added className Prop

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,9 +1,10 @@
 import { FaConfluence, FaDiscord, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { SiJira } from 'react-icons/si'
 import Link from 'next/link'
+import { clsx } from 'clsx'
 
-export default function SocialIcons() {
-  return <div className="flex items-center justify-center w-full gap-4 text-2xl text-green">
+export default function SocialIcons({ className }: { className?: string }) {
+  return <div className={clsx('flex items-center justify-center w-full gap-4 text-2xl text-green', className)}>
     <Link target="_blank" href={'https://www.linkedin.com/company/afetyonetimsistemi/'}><FaLinkedin /></Link>
     <Link target="_blank" href={'https://discord.gg/NkAkYajkKa'}><FaDiscord /></Link>
     <Link target="_blank" href={'https://afetyonetimsistemi.atlassian.net/jira'}><SiJira /></Link>
