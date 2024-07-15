d4e62d6c88a5f3f256ceaf7dba514d1c1f1231ee
NNakreSS <serkanatmaca15@gmail.com>
2024-07-15 03:41:12 +0300

    refactor : array map used for navigation links

--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -3,20 +3,26 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { clsx } from 'clsx'
-import { AiOutlineHome } from 'react-icons/ai'
 import { useTranslation } from 'react-i18next'
+import Navigations from '@/components/Header/Navigations'
 
 export default function HeaderNav() {
   const pathName = usePathname()
   const { t } = useTranslation()
   return (
-    <nav className="flex items-center justify-between font-normal text-normal">
-      <Link href="/" className={clsx('flex-center gap-2 py-1', {
-        'border-b-2': pathName === '/' || pathName === '/home'
-      })}>
-        <AiOutlineHome />
-        <span>{t('nav.home')}</span>
-      </Link>
+    <nav className="items-center justify-between font-normal text-normal hidden lg:flex">
+      {
+        Navigations.map(({ pathName: path, title, Icon }) =>
+          <Link href={path} key={path} className={clsx('flex-center gap-2 py-1',
+            {
+              'border-b-2': pathName === path
+            }
+          )}>
+            <Icon />
+            <span>{t(`nav.${title}`)}</span>
+          </Link>
+        )
+      }
     </nav>
   )
 }
