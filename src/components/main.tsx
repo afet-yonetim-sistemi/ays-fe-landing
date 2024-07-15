78e4dfb5c74166544558a7c81a656f828b1b32df
NNakreSS <serkanatmaca15@gmail.com>
2024-07-15 12:33:47 +0300

    responsive space height

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,5 @@
 export default function Main({ children }: { children: React.ReactNode }) {
-  return <main className="flex flex-col items-center justify-start space-y-10">
+  return <main className="flex flex-col items-center justify-start space-y-4 md:space-y-10">
     {children}
   </main>
 }
