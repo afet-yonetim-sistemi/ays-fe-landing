5f273a4aa43a47d0d45e80a743dc1168a382f4ae
NNakreSS <serkanatmaca15@gmail.com>
2024-08-01 21:31:30 +0300

    initial commit

---
+++ src/components/ui/PhoneInput.tsx
@@ -0,0 +1,15 @@
+import ReactPhoneInput, { PhoneInputProps } from 'react-phone-input-2'
+
+export default function PhoneInput(props: PhoneInputProps) {
+  return <ReactPhoneInput
+    {...props}
+    containerClass="w-full"
+    country={'tr'}
+    placeholder="Telefon Numarası"
+    enableSearch={true}
+    searchClass="bg-white "
+    searchPlaceholder="Ara..."
+    searchNotFound="Bulunamadı"
+    inputClass="py-5 !w-full"
+  />
+}
