294545670bd084a276fddc5457383f06cd6a7a0a
NNakreSS <serkanatmaca15@gmail.com>
2024-08-02 23:27:58 +0300

    fixed turkish char error

--- src/components/EmergencyForm/schema/formSchema.ts
+++ src/components/EmergencyForm/schema/formSchema.ts
@@ -9,22 +9,22 @@ const formSchema = z.object({
   applicantFirstName: z.string()
     .min(2, { message: 'Minimum 2 karakter uzunluğunda olmalıdır' })
     .max(100, { message: 'Maksimum 100 karakter uzunluğunda olabilir' })
-    .regex(/^[a-zA-Z\s]*$/, { message: 'Özel karakter içeremez' })
+    .regex(/^[a-zA-ZğüşıöçĞÜŞİÖÇ\s]*$/, { message: 'Özel karakter içeremez' })
     .optional(),
   applicantLastName: z.string()
     .min(2, { message: 'Minimum 2 karakter uzunluğunda olmalıdır' })
     .max(100, { message: 'Maksimum 100 karakter uzunluğunda olabilir' })
-    .regex(/^[a-zA-Z\s]*$/, { message: 'Özel karakter içeremez' })
+    .regex(/^[a-zA-ZğüşıöçĞÜŞİÖÇ\s]*$/, { message: 'Özel karakter içeremez' })
     .optional(),
   applicantPhoneNumber: phoneNumberSchema.optional(),
   firstName: z.string()
     .min(2, { message: 'Minimum 2 karakter uzunluğunda olmalıdır' })
     .max(100, { message: 'Maksimum 100 karakter uzunluğunda olabilir' })
-    .regex(/^[a-zA-Z\s]*$/, { message: 'Özel karakter içeremez' }),
+    .regex(/^[a-zA-ZğüşıöçĞÜŞİÖÇ\s]*$/, { message: 'Özel karakter içeremez' }),
   lastName: z.string()
     .min(2, { message: 'Minimum 2 karakter uzunluğunda olmalıdır' })
     .max(100, { message: 'Maksimum 100 karakter uzunluğunda olabilir' })
-    .regex(/^[a-zA-Z\s]*$/, { message: 'Özel karakter içeremez' }),
+    .regex(/^[a-zA-ZğüşıöçĞÜŞİÖÇ\s]*$/, { message: 'Özel karakter içeremez' }),
   phoneNumber: phoneNumberSchema,
   sourceCity: z.string()
     .min(1, { message: 'Bir şehir seçmelisin' })
