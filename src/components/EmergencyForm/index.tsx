04e96bbd46c0f622fed81294fb2230e2d00d97b4
NNakreSS <serkanatmaca15@gmail.com>
2024-08-08 13:20:27 +0300

    refactor: responsive style font for tabsTrigger

--- src/components/EmergencyForm/index.tsx
+++ src/components/EmergencyForm/index.tsx
@@ -10,12 +10,12 @@ import OtherEvacuationForm from '@/components/EmergencyForm/OtherEvacuationForm'
 export default function EmergencyForm() {
   return <div className="w-full bg-nightBlue rounded-2xl p-5 space-y-5">
     <div className="border-b border-background text-4xl font-medium flex-center p-5">
-      <h3>Acil Durum Tahliye Başvurusu</h3>
+      <h3 className="text-center">Acil Durum Tahliye Başvurusu</h3>
     </div>
     <Tabs defaultValue="self" className="w-full">
       <TabsList className="grid w-full grid-cols-2 h-auto text-nightBlue">
-        <TabsTrigger value="self">Kendi Adıma</TabsTrigger>
-        <TabsTrigger value="other">Başkası Adına</TabsTrigger>
+        <TabsTrigger value="self" className="text-lg">Kendi Adıma</TabsTrigger>
+        <TabsTrigger value="other" className="text-lg">Başkası Adına</TabsTrigger>
       </TabsList>
       <TabsContent value="self">
         <SelfEvacuationForm />
