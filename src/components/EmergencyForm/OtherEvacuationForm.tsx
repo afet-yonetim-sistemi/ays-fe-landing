cc72fa3afeb57cab74ca0ad901995de22b393e0c
NNakreSS <serkanatmaca15@gmail.com>
2024-08-14 19:42:53 +0300

    common form structure moved to single component

--- src/components/EmergencyForm/OtherEvacuationForm.tsx
+++ src/components/EmergencyForm/OtherEvacuationForm.tsx
@@ -16,6 +16,7 @@ import { useState } from 'react'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { FiAlertTriangle } from 'react-icons/fi'
 import onSubmitForm from '@/services/formService'
+import EvacuationForm from '@/components/EmergencyForm/EvacuationForm'
 
 export default function OtherEvacuationForm() {
   const [loading, setLoading] = useState<boolean>(false)
@@ -40,8 +41,6 @@ export default function OtherEvacuationForm() {
     }
   })
 
-  const countryCodeError = form.getFieldState('phoneNumber.countryCode', form.formState).error
-  const lineNumberError = form.getFieldState('phoneNumber.lineNumber', form.formState).error
   const applicantCountryCodeError = form.getFieldState('applicantPhoneNumber.countryCode', form.formState).error
   const applicantLineNumberError = form.getFieldState('applicantPhoneNumber.lineNumber', form.formState).error
 
@@ -111,163 +110,26 @@ export default function OtherEvacuationForm() {
                 </FormItem>
               )} />
 
-              <div className="grid grid-cols-1 gap-2">
-                <Alert variant="default" className="bg-transparent border-orange-500 text-backgorund">
-                  <FiAlertTriangle className="text-xl !text-orange-500" />
-                  <AlertTitle className="font-semibold text-orange-500">Dikkat</AlertTitle>
-                  <AlertDescription className="w-full text-orange-500 ">
-                    Formun bu kısmını kendinize ait bilgiler ile doldurarak diğer adıma geçin!
-                  </AlertDescription>
-                </Alert>
-                <Button disabled={loading} onClick={nextStep} className="!bg-submitBlue float-end text-lg">
-                  ileri
-                </Button>
-              </div>
-            </> : <> {/* step 2 */}
-              {/* name surname block*/}
-              <div className="grid grid-cols-2 gap-2">
-                <FormField key="firstName" name="firstName" render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input placeholder="İsim" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
+              <Alert variant="default" className="bg-transparent border-orange-500 text-backgorund">
+                <FiAlertTriangle className="text-xl !text-orange-500" />
+                <AlertTitle className="font-semibold text-orange-500">Dikkat</AlertTitle>
+                <AlertDescription className="w-full text-orange-500 ">
+                  Formun bu kısmını kendinize ait bilgiler ile doldurarak diğer adıma geçin!
+                </AlertDescription>
+              </Alert>
 
-                <FormField key="lastName" name="lastName" render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input placeholder="Soyisim" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
-              </div>
-
-              {/*Phone Number block*/}
-              <FormField key="phoneNumber" name="phoneNumber" render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <PhoneInput
-                      value={field.value.countryCode + field.value.lineNumber}
-                      onChange={(value: string, country: CountryData) => {
-                        const countryCode: string = country.dialCode
-                        const lineNumber: string = value.slice(countryCode.length)
-                        field.onChange({ countryCode, lineNumber })
-                      }}
-                    />
-                  </FormControl>
-                  {/*<FormMessage />*/}
-                  <div className="text-red-500 font-thin text-sm flex gap-2">
-                    <span>{countryCodeError?.message}</span>
-                    <span>{lineNumberError?.message}</span>
-                  </div>
-                </FormItem>
-              )} />
-
-              {/*number of seats block*/}
-              <FormField name="seatingCount" render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <div className="flex items-center justify-between gap-5">
-                      <Label htmlFor="seatingCount" className="text-background text-nowrap font-semibold">Koltuk
-                        Sayısı:</Label>
-                      <Input
-                        id="seatingCount"
-                        placeholder="Koltuk Sayısı"
-                        type="number"
-                        min={0}
-                        max={999}
-                        {...field}
-                        onChange={(e) => field.onChange(Number(e.target.value))}
-                      />
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )} />
-
-              <hr />
-              <span className="font-bold text-white block text-center">Başvurunun Yapıldığı Konum</span>
-              {/*application location box*/}
-              <div className="grid grid-cols-2 gap-2">
-                <FormField name="sourceCity" render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <SelectLocation
-                        type="city"
-                        value={field.value}
-                        onChange={field.onChange}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
-
-                <FormField name="sourceDistrict" render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <SelectLocation
-                        type="district"
-                        cityValue={form.watch('sourceCity')}
-                        value={field.value}
-                        onChange={field.onChange}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
-              </div>
-
-              <FormField name="address" render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea {...field} placeholder="Açık adres" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )} />
-
-              <hr />
-              <span className="font-bold text-white block text-center">Tahliye Sağlanacak Konum</span>
-
-              {/*target location box*/}
-              <div className="grid grid-cols-2 gap-2">
-                <FormField name="targetCity" render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <SelectLocation
-                        type="city"
-                        value={field.value}
-                        onChange={field.onChange}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
-
-                <FormField name="targetDistrict" render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <SelectLocation
-                        type="district"
-                        cityValue={form.watch('targetCity')}
-                        value={field.value}
-                        onChange={field.onChange}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
-              </div>
-
-              <div className="grid grid-cols-2 gap-2">
-                <Button disabled={loading} onClick={prevStep} className="!bg-submitBlue w-full text-lg">
+              <Button disabled={loading} onClick={nextStep} className="!bg-submitBlue float-end text-lg w-full mt-2">
+                ileri
+              </Button>
+            </> :
+            <>
+              <EvacuationForm form={form} />
+              <div className="w-full grid grid-cols-2 gap-2">
+                <Button disabled={loading} onClick={prevStep} className="!bg-submitBlue text-lg">
                   Geri
                 </Button>
-                <Button disabled={loading} type="submit" className="!bg-submitBlue w-full text-lg">
-                  {loading ? 'Gönderiliyor...' : 'Gönder'}
+                <Button disabled={loading} type="submit" className="!bg-submitBlue text-lg">
+                  Gönder
                 </Button>
               </div>
             </>
