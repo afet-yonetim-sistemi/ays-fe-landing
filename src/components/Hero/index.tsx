8610809e84c00896a78c5121cd347e884abf0286
gafur-demirci <gafur.gs.68@gmail.com>
2024-09-14 23:17:34 +0300

    Removed logs

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,10 +4,8 @@ import EmergencyForm from '@/components/EmergencyForm'
 import nextConfig from '../../../next.config.mjs'
 
 export default function HeroSection() {
-  console.log("next config");
-  console.log(nextConfig.env?.showEmergency);
+  
   const showEmergency = nextConfig.env?.showEmergency === 'true'
-
   const contClassByConfig = showEmergency === true ? "grid grid-cols-1 lg:grid-cols-2 gap-5" : "grid grid-cols-1 lg:grid-cols-1 gap-5"
 
   return (
