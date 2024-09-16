e85246abeb90bb7c831c7c1a62ef7cd059b9c492
gafur-demirci <gafur.gs.68@gmail.com>
2024-09-16 18:29:54 +0300

    production env deleted, development env key changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        showEmergency: process.env.NEXT_PUBLIC_SHOW_EMERGENCY
+        showEmergencyForm: process.env.NEXT_PUBLIC_SHOW_EMERGENCY_FORM
     }
 };
 
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,14 +5,14 @@ import nextConfig from '../../../next.config.mjs'
 
 export default function HeroSection() {
   
-  const showEmergency = nextConfig.env?.showEmergency === 'true'
-  const contClassByConfig = showEmergency === true ? "grid grid-cols-1 lg:grid-cols-2 gap-5" : "grid grid-cols-1 lg:grid-cols-1 gap-5"
+  const showEmergencyForm = nextConfig.env?.showEmergencyForm === 'true'
+  const contClassByConfig = showEmergencyForm === true ? "grid grid-cols-1 lg:grid-cols-2 gap-5" : "grid grid-cols-1 lg:grid-cols-1 gap-5"
 
   return (
     <section className="w-full bg-green py-5 lg:px-5">
       <Container className={contClassByConfig}>
         <HeroCarousel />
-        {showEmergency && <EmergencyForm />}
+        {showEmergencyForm && <EmergencyForm />}
       </Container>
     </section>
   )
