dc17a5317249b710f02b22280652f4040e14b30e
gafur-demirci <gafur.gs.68@gmail.com>
2024-09-17 21:28:54 +0300

    AYS-415 | Environment config files added for production & development and used … (#74)
    Co-authored-by: Buğra Ercan <bugrasshgercan@gmail.com>

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,13 +2,21 @@ import HeroCarousel from './HeroCarousel'
 import Container from '@/components/Container'
 import EmergencyForm from '@/components/EmergencyForm'
 
-export default function HeroSection() {
+const HeroSection: React.FC = () => {
+  const showEmergencyForm = process.env.NEXT_PUBLIC_ENVIRONMENT === 'test'
+
   return (
     <section className="w-full bg-green py-5 lg:px-5">
-      <Container className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+      <Container
+        className={`grid gap-5 grid-cols-1 ${
+          showEmergencyForm ? 'lg:grid-cols-2' : 'lg:grid-cols-1'
+        }`}
+      >
         <HeroCarousel />
-        <EmergencyForm />
+        {showEmergencyForm && <EmergencyForm />}
       </Container>
     </section>
   )
 }
+
+export default HeroSection
