5396feecc2966e303068acd652bbc135f272dfc2
Agit Rubar Demir <61833677+agitrubard@users.noreply.github.com>
2025-10-05 20:51:25 +0300

    AYS-875 | New `/api/landing/v1/**` Base Path Has Been Defined (#108)

--- src/services/formService.ts
+++ src/services/formService.ts
@@ -18,7 +18,10 @@ const onSubmitForm = async (
   cb: () => void
 ): Promise<void> => {
   try {
-    const { data } = await http.post('emergency-evacuation-application', values)
+    const { data } = await http.post(
+      '/api/landing/v1/emergency-evacuation-application',
+      values
+    )
     if (data.isSuccess) {
       toast({
         variant: 'success',
@@ -26,13 +29,13 @@ const onSubmitForm = async (
         description: 'Başvurunuz kontrol edilmek üzere başarıyla alınmıştır',
       })
       cb()
-    } else {
-      toast({
-        variant: 'destructive',
-        title: 'İşlem Başarısız',
-        description: 'İşlem sırasında bir hata meydana geldi',
-      })
+      return
     }
+    toast({
+      variant: 'destructive',
+      title: 'İşlem Başarısız',
+      description: 'İşlem sırasında bir hata meydana geldi',
+    })
   } catch (err) {
     handleFormErrors(err as AxiosError<ErrorResponse>)
   }
