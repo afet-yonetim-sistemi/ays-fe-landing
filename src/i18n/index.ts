7c06626bece535d013d94d90d7350e910b227f93
Abdullah KUŞ <abdullahkuscom@gmail.com>
2025-10-28 17:26:20 +0300

    AYS-817 | i18n Has Been Refactored

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,25 +1,7 @@
 import i18n from 'i18next'
-import localeTR from './locales/tr.json'
+import { i18nConfig } from './config'
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
-  tr: {
-    translation: localeTR,
-  },
-}
-
-i18n.init({
-  resources,
-  fallbackLng: 'tr',
-  lng: 'tr', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-  // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-  // if you're using a language detector, do not define the lng option
-  interpolation: {
-    escapeValue: false, // react already safes from xss
-  },
-  debug: process.env.NODE_ENV === 'development',
-})
+i18n.init(i18nConfig)
 
 export default i18n
+export * from './resources'
