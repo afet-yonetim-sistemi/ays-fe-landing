781c54a969d329184ed91ca1524556c76a987e8d
Abdullah KUŞ <abdullahkuscom@gmail.com>
2026-02-03 23:17:13 +0300

    AYS-1104 | lint-staged Config Has Been Edited

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,8 @@
 const config = {
-  '**/*': ['prettier --write', 'eslint --fix --max-warnings 0'],
+  '**/*': [
+    'prettier --write',
+    'eslint --fix --max-warnings 0 --no-warn-ignored',
+  ],
 }
 
 export default config
